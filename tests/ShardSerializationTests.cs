8eb909305f1bbea3289435b2a72f2f151904cb93
John Hicks <johnhicks@compte.com>
2025-02-24 20:32:05 -0800

    Updated to .Net 9, added Orleans files from implementation.

--- tests/ShardSerializationTests.cs
+++ tests/ShardSerializationTests.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using ArgentSea;
 using FluentAssertions;
+using System.Text;
+using System.Runtime;
 
 namespace ArgentSea.Test
 {
@@ -15,6 +17,28 @@ namespace ArgentSea.Test
             var str = sk1.ToExternalString();
             var sk2 = ShardKey<short>.FromExternalString(str);
             sk2.Should().Be(sk1, "because the serialized string creates an equivalent shardKey");
+        }
+        [Fact]
+        public void TestShardKeySerializationInts3()
+        {
+            var sk1 = new ShardKey<short>('a', 3, 4);
+            var array = sk1.ToArray();
+            var sk2 = new ShardKey<short>(array);
+            sk2.Should().Be(sk1, "because the serialized array creates an equivalent shardKey");
+        }
+        [Fact]
+        public void TestOreleansShardKeySerializationInts4()
+        {
+            var sk1 = new ShardKey<Guid>('s', 0, Guid.NewGuid());
+            //var span = new Span<byte>()
+            ReadOnlyMemory<byte> rom = sk1.ToUtf8();
+            var sk2 = ShardKey<Guid>.FromUtf8(rom.Span);
+            sk2.Should().Be(sk1, "because the serialized array creates an equivalent shardKey");
+            var str = Encoding.UTF8.GetString(rom.Span);
+            var utf8 = Encoding.UTF8.GetBytes(str);
+            //var sk3 = new ShardKey<Guid>(utf8);
+            var sk3 = ShardKey<Guid>.FromUtf8(utf8);
+            sk3.Should().Be(sk1, "because the serialized array creates an equivalent shardKey");
         }
         [Fact]
         public void TestShardKeySerializationStr()
